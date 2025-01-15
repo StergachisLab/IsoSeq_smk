0666497e52d1b65875476a8aa5504d507ca97a4a
adrisede <adrisede@gmail.com>
2025-01-15 10:57:35 -0800

    Update create_dictionary.R

--- scripts/create_dictionary.R
+++ scripts/create_dictionary.R
@@ -17,12 +17,13 @@ process_collapsed <- function(filepath) {
 }
 
 # Function to read and process haplotag files
-process_haplotag <- function(files) {
-  haplotag_data <- files %>%
+process_haplotag <- function(directory) {
+  haplotag_files <- list.files(directory, pattern = "haplotagged.txt", full.names = TRUE)
+  haplotag_data <- haplotag_files %>%
     set_names() %>%
     purrr::map_dfr(
-      ~ data.table::fread(.x, header = TRUE, stringsAsFactors = FALSE, check.names = TRUE) %>% 
-        dplyr::mutate(Filename = .x), 
+      ~ data.table::fread(.x, header = TRUE, stringsAsFactors = FALSE, check.names = TRUE) %>%
+        dplyr::mutate(Filename = .x),
       .id = "Filename"
     )
   return(haplotag_data)
@@ -32,7 +33,7 @@ process_haplotag <- function(files) {
 process_classification <- function(filepath) {
   classification_data <- read.table(filepath, header = TRUE, sep = "\t") %>%
     #janitor::row_to_names(row_number = 1) %>%
-    select(isoform, structural_category, associated_gene, associated_transcript, subcategory)
+    dplyr::select(isoform, structural_category, associated_gene, associated_transcript, subcategory)
   return(classification_data)
 }
 
@@ -40,43 +41,21 @@ process_classification <- function(filepath) {
 create_dictionary <- function(collapsed_file, haplotag_files, classification_file) {
   # Process the input files
   collapsed_data <- process_collapsed(collapsed_file)
-  haplotag_data <- process_haplotag(haplotag_files)
+  haplotag_data <- process_haplotag(haplotag_dir)
   classification_data <- process_classification(classification_file)
-  
+
   dictionary <- collapsed_data %>%
     dplyr::left_join(classification_data) %>%
-    dplyr::left_join(haplotag_data, by = join_by("read_id" == "ReadName")) 
-  
-counts_hap <- dictionary %>%
-  group_by(isoform, condition, haplotype) %>%
-  count() %>%
-  mutate(n_hap_cond = n) %>%
-  select(-n) %>%
-  pivot_wider(id_cols = c(isoform,Haplotype), names_from = c(condition), values_from = n_hap_cond) %>%
-  mutate(iso_hap_noncyclo_counts = paste0("hn:i:", `non-cyclo`),
-         iso_hap_cyclo_counts = paste0("hc:i:", cyclo)) %>%
-  select(-cyclo, -`non-cyclo`)
-
-counts <- dictionary %>%
-  group_by(isoform, condition) %>%
-  count() %>%
-  mutate(n_cond = n) %>%
-  select(-n) %>%
-  pivot_wider(isoform, names_from = condition, values_from = n_cond) %>%
-  mutate(iso_noncyclo_counts = paste0("sn:i:", `non-cyclo`),
-         iso_cyclo_counts = paste0("sc:i:", cyclo)) %>%
-  select(-cyclo, -`non-cyclo`)
-
-dictionary <- left_join(dictionary, counts_hap) %>%
-  left_join(counts) %>%
-  mutate(isoform = gsub("PB.", "in:Z:", isoform),
-         structural_category = paste0("sc:Z:",structural_category),
-         associated_gene = paste0("gn:Z:", associated_gene),
-         associated_transcript = paste0("tn:Z:", associated_transcript),
-         subcategory = paste0("sb:Z:", subcategory)) %>%
+    dplyr::left_join(haplotag_data, by = join_by("read_id" == "ReadName")) %>%
+    dplyr::mutate(isoform = gsub("PB.", "in:Z:", isoform),
+           structural_category = paste0("sc:Z:",structural_category),
+           associated_gene = paste0("gn:Z:", associated_gene),
+           associated_transcript = paste0("tn:Z:", associated_transcript),
+           subcategory = paste0("sb:Z:", subcategory)) %>%
     distinct(.) %>%
     dplyr::select(read_id, haplotype, isoform, structural_category, associated_gene, associated_transcript, subcategory)
-  
+
+
   # Write the dictionary to a file
   dir.create("tag")
   write.table(dictionary, file = "tag/dictionary_all.txt", sep = "\t", row.names = FALSE, quote = FALSE)
@@ -85,8 +64,8 @@ dictionary <- left_join(dictionary, counts_hap) %>%
 # Parse command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
 collapsed_file <- args[1]
-haplotag_files <- args[2]
+haplotag_dir <- args[2]
 classification_file <- args[3]
 
 # Call the main function
-create_dictionary(collapsed_file, haplotag_files, classification_file)
+create_dictionary(collapsed_file, haplotag_dir, classification_file)
