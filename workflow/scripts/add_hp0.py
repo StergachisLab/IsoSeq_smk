1247494eea008754c31c48242d5baee183af7d58
adrisede <adrisede@gmail.com>
2025-07-21 11:32:12 -0700

    lint

--- workflow/scripts/add_hp0.py
+++ workflow/scripts/add_hp0.py
@@ -3,15 +3,17 @@
 import sys
 import pysam
 
+
 def add_hp_tag(input_bam, output_bam):
     with pysam.AlignmentFile(input_bam, "rb") as in_bam:
         with pysam.AlignmentFile(output_bam, "wb", header=in_bam.header) as out_bam:
             for read in in_bam:
                 if not read.has_tag("HP"):
-                    read.set_tag("HP", 0, value_type='i')
+                    read.set_tag("HP", 0, value_type="i")
                 out_bam.write(read)
     pysam.index(output_bam)
     print("Successfully wrote {} with HP:i:0 tag and created index.".format(output_bam))
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
